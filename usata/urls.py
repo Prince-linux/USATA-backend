c049d7fe0f40207ad44c49e9e80900fce7070914
Prince <regioths@gmail.com>
2025-07-15 22:27:12 +0000

    Requested changes

--- usata/urls.py
+++ usata/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from core.admin import admin_site
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    # path("admin/", admin.site.urls),
+    path('admin/', admin_site.urls),
     path('api/', include('core.urls')),
 ]
